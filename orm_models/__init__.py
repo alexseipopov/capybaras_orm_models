03d0e3f584de5f5dc35135bff3d8eb897da5a7f7
Alexsei Popov <popov2895@gmail.com>
2024-01-02 18:42:14 +0300

    add UserAvatar into common list

--- orm_models/__init__.py
+++ orm_models/__init__.py
@@ -2,6 +2,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-from orm_models.user import User, UserAccess
+from orm_models.user import User, UserAccess, UserAvatar
 from orm_models.capybaras import Capybara
 from orm_models.election import Election, Vote, Candidate, Tribe
